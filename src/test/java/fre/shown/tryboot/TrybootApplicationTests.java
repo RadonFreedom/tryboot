123cdb27dc4cbbb21ccf306a6fa62a667c758ddb
Radon Freedom <Radon.Freedom@gmail.com>
2019-03-01 08:25:17 +0800

    '变更使用yaml格式配置文件'

--- src/test/java/fre/shown/tryboot/TrybootApplicationTests.java
+++ src/test/java/fre/shown/tryboot/TrybootApplicationTests.java
@@ -1,7 +1,9 @@
 package fre.shown.tryboot;
 
+import fre.shown.tryboot.bean.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TrybootApplicationTests {
 
+    @Autowired
+    private Account account;
+
     @Test
     public void contextLoads() {
+
+        System.out.println(account);
     }
 
 }
