27eae7a853f41cd169fd944bb966f859a91079be
Radon <RadonFreedom@github.com>
2019-03-23 12:57:33 +0800

    修改前端页面

--- cdn/src/main/resources/static/js/auth.js
+++ cdn/src/main/resources/static/js/auth.js
@@ -8,7 +8,6 @@ var Auth = {
 		login_back_link: document.querySelector('.login-back-link'),
 		forgot_link: document.querySelector('.forgot-link'),
 		login_link: document.querySelector('.login-link'),
-		login_btn: document.querySelector('.login-btn'),
 		register_link: document.querySelector('.register-link'),
 		password_group: document.querySelector('.password-group'),
 		password_group_height: 0,
@@ -38,7 +37,7 @@ var Auth = {
 		setTimeout(() => {
 			Auth.vars.lowin_register.style.display = 'none';
 		}, 500);
-		
+
 		setTimeout(() => {
 			Auth.vars.lowin_register.classList.remove('lowin-animated-flipback');
 			Auth.vars.lowin_login.classList.remove('lowin-animatedback');
@@ -46,46 +45,6 @@ var Auth = {
 
 		Auth.setHeight(Auth.vars.lowin_login.offsetHeight + Auth.vars.lowin_footer.offsetHeight);
 
-		e.preventDefault();
-	},
-	forgot(e) {
-		Auth.vars.password_group.classList += ' lowin-animated';
-		Auth.vars.login_back_link.style.display = 'block';
-
-		setTimeout(() => {
-			Auth.vars.login_back_link.style.opacity = 1;
-			Auth.vars.password_group.style.height = 0;
-			Auth.vars.password_group.style.margin = 0;
-		}, 100);
-		
-		Auth.vars.login_btn.innerText = 'Forgot Password';
-
-		Auth.setHeight(Auth.vars.lowin_wrapper_height - Auth.vars.password_group_height);
-		Auth.vars.lowin_login.querySelector('form').setAttribute('action', Auth.vars.option.forgot_url);
-
-		e.preventDefault();
-	},
-	loginback(e) {
-		Auth.vars.password_group.classList.remove('lowin-animated');
-		Auth.vars.password_group.classList += ' lowin-animated-back';
-		Auth.vars.password_group.style.display = 'block';
-
-		setTimeout(() => {
-			Auth.vars.login_back_link.style.opacity = 0;
-			Auth.vars.password_group.style.height = Auth.vars.password_group_height + 'px';
-			Auth.vars.password_group.style.marginBottom = 30 + 'px';
-		}, 100);
-
-		setTimeout(() => {
-			Auth.vars.login_back_link.style.display = 'none';
-			Auth.vars.password_group.classList.remove('lowin-animated-back');
-		}, 1000);
-
-		Auth.vars.login_btn.innerText = 'Sign In';
-		Auth.vars.lowin_login.querySelector('form').setAttribute('action', Auth.vars.option.login_url);
-
-		Auth.setHeight(Auth.vars.lowin_wrapper_height);
-		
 		e.preventDefault();
 	},
 	setHeight(height) {
@@ -104,9 +63,6 @@ var Auth = {
 		Auth.vars.password_group_height = Auth.vars.password_group.offsetHeight;
 		Auth.vars.lowin_wrapper_height = Auth.vars.lowin_wrapper.offsetHeight;
 
-		Auth.vars.option = option;
-		Auth.vars.lowin_login.querySelector('form').setAttribute('action', option.login_url);
-
 		var len = Auth.vars.box.length - 1;
 
 		for(var i = 0; i <= len; i++) {
@@ -114,10 +70,6 @@ var Auth = {
 				Auth.vars.box[i].className += ' lowin-flip';
 			}
 		}
-
-		Auth.vars.forgot_link.addEventListener("click", (e) => {
-			Auth.forgot(e);
-		});
 
 		Auth.vars.register_link.addEventListener("click", (e) => {
 			Auth.brand();
@@ -127,10 +79,6 @@ var Auth = {
 		Auth.vars.login_link.addEventListener("click", (e) => {
 			Auth.brand();
 			Auth.login(e);
-		});
-
-		Auth.vars.login_back_link.addEventListener("click", (e) => {
-			Auth.loginback(e);
 		});
 	}
 }
