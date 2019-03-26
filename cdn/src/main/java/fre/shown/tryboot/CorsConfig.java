a8677f0f62c1553f856b0c1b5d27af7977437495
RadonFreedom <radon.freedom@gmail.com>
2019-03-26 18:30:06 +0800

    扩张CDN服务

---
+++ cdn/src/main/java/fre/shown/tryboot/CorsConfig.java
@@ -0,0 +1,20 @@
+package fre.shown.tryboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Radon Freedom
+ * created at 2019.03.26 下午4:21
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
