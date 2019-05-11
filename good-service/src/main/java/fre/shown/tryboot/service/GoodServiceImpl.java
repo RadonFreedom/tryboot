2726604596e6fcaea517b719ea511e401db3800d
“RadonFreedom” <radon.freedom@gmail.com>
2019-05-11 16:09:44 +0800

    commit

--- good-service/src/main/java/fre/shown/tryboot/service/GoodServiceImpl.java
+++ good-service/src/main/java/fre/shown/tryboot/service/GoodServiceImpl.java
@@ -33,6 +33,7 @@ public class GoodServiceImpl implements GoodService {
     @Override
     public ResultVO<SeckillGoodDetailVO> getSeckillGoodById(Long seckillGoodId) {
 
+
         ResultVO<SeckillGoodDetailVO> resultVO = new ResultVO<>();
 
         Boolean hasKey = redisService.hasKey(seckillGoodId, SeckillGoodDTO.class);
